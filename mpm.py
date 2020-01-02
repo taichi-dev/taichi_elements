1f95bbba73432f3a9e82ca45712de91bb466e3ae
Yuanming Hu <yuanmhu@gmail.com>
2020-01-02 17:33:56 -0500

    more oop in mpm.py

--- mpm.py
+++ mpm.py
@@ -2,12 +2,6 @@ import taichi as ti
 import random
 import numpy as np
 
-quality = 1  # Use a larger value for higher-res simulations
-n_particles, n_grid = 9000 * quality**2, 128 * quality
-dx, inv_dx = 1 / n_grid, float(n_grid)
-dt = 1e-4 / quality
-p_vol, p_rho = (dx * 0.5)**2, 1
-p_mass = p_vol * p_rho
 E, nu = 0.1e4, 0.2  # Young's modulus and Poisson's ratio
 mu_0, lambda_0 = E / (2 * (1 + nu)), E * nu / (
     (1 + nu) * (1 - 2 * nu))  # Lame parameters
@@ -16,35 +10,50 @@ ti.cfg.arch = ti.cuda  # Try to run on GPU
 
 class MPMSolver:
 
-  def __init__(self):
-    self.x = ti.Vector(2, dt=ti.f32, shape=n_particles)  # position
-    self.v = ti.Vector(2, dt=ti.f32, shape=n_particles)  # velocity
-    self.C = ti.Matrix(
-        2, 2, dt=ti.f32, shape=n_particles)  # affine velocity field
-    self.F = ti.Matrix(
-        2, 2, dt=ti.f32, shape=n_particles)  # deformation gradient
-    self.material = ti.var(dt=ti.i32, shape=n_particles)  # material id
-    self.Jp = ti.var(dt=ti.f32, shape=n_particles)  # plastic deformation
-    self.grid_v = ti.Vector(
-        2, dt=ti.f32, shape=(n_grid, n_grid))  # grid node momemtum/velocity
-    self.grid_m = ti.var(dt=ti.f32, shape=(n_grid, n_grid))  # grid node mass
+  def __init__(self, res):
+    self.dim = len(res)
+    self.res = res
+    self.n_particles = 9000
+    self.dx = 1 / res[0]
+    self.dt = 1e-4
+    self.inv_dx = float(res[0])
+    self.p_vol = self.dx**self.dim
+    self.p_rho = 1
+    self.p_mass = self.p_vol * self.p_rho
+    # position
+    self.x = ti.Vector(2, dt=ti.f32, shape=self.n_particles)
+    # velocity
+    self.v = ti.Vector(2, dt=ti.f32, shape=self.n_particles)
+    # affine velocity field
+    self.C = ti.Matrix(2, 2, dt=ti.f32, shape=self.n_particles)
+    # deformation gradient
+    self.F = ti.Matrix(2, 2, dt=ti.f32, shape=self.n_particles)
+    # material id
+    self.material = ti.var(dt=ti.i32, shape=self.n_particles)
+    # plastic deformation
+    self.Jp = ti.var(dt=ti.f32, shape=self.n_particles)
+    # grid node momemtum/velocity
+    self.grid_v = ti.Vector(2, dt=ti.f32, shape=self.res)
+    # grid node mass
+    self.grid_m = ti.var(dt=ti.f32, shape=self.res)
 
   @ti.classkernel
   def substep(self):
-    for i, j in ti.ndrange(n_grid, n_grid):
+    for i, j in ti.ndrange(*self.res):
       self.grid_v[i, j] = [0, 0]
       self.grid_m[i, j] = 0
 
     # Particle state update and scatter to grid (P2G)
-    for p in range(n_particles):
-      base = (self.x[p] * inv_dx - 0.5).cast(int)
-      fx = self.x[p] * inv_dx - base.cast(float)
+    for p in range(self.n_particles):
+      base = (self.x[p] * self.inv_dx - 0.5).cast(int)
+      fx = self.x[p] * self.inv_dx - base.cast(float)
       # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
       w = [
           0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1), 0.5 * ti.sqr(fx - 0.5)
       ]
+      # deformation gradient update
       self.F[p] = (ti.Matrix.identity(ti.f32, 2) +
-                   dt * self.C[p]) @ self.F[p]  # deformation gradient update
+                   self.dt * self.C[p]) @ self.F[p]
       # Hardening coefficient: snow gets harder when compressed
       h = ti.exp(10 * (1.0 - self.Jp[p]))
       if self.material[p] == 1:  # jelly, make it softer
@@ -68,35 +77,38 @@ class MPMSolver:
         # Reconstruct elastic deformation gradient after plasticity
         self.F[p] = U @ sig @ V.T()
 
-      stress = 2 * mu * (self.F[p] - U @ V.T()) @ self.F[p].T() + ti.Matrix.identity(
-          ti.f32, 2) * la * J * (
-              J - 1)
-      stress = (-dt * p_vol * 4 * inv_dx * inv_dx) * stress
-      affine = stress + p_mass * self.C[p]
+      stress = 2 * mu * (self.F[p] - U @ V.T()) @ self.F[p].T(
+      ) + ti.Matrix.identity(ti.f32, 2) * la * J * (
+          J - 1)
+      stress = (-self.dt * self.p_vol * 4 * self.inv_dx**2) * stress
+      affine = stress + self.p_mass * self.C[p]
 
       # Loop over 3x3 grid node neighborhood
       for i, j in ti.static(ti.ndrange(3, 3)):
         offset = ti.Vector([i, j])
-        dpos = (offset.cast(float) - fx) * dx
+        dpos = (offset.cast(float) - fx) * self.dx
         weight = w[i][0] * w[j][1]
-        self.grid_v[base + offset] += weight * (p_mass * self.v[p] + affine @ dpos)
-        self.grid_m[base + offset] += weight * p_mass
+        self.grid_v[base + offset] += weight * (
+            self.p_mass * self.v[p] + affine @ dpos)
+        self.grid_m[base + offset] += weight * self.p_mass
 
-    for i, j in ti.ndrange(n_grid, n_grid):
+    for i, j in ti.ndrange(*self.res):
       if self.grid_m[i, j] > 0:  # No need for epsilon here
-        self.grid_v[i, j] = (1 / self.grid_m[i, j]) * self.grid_v[i, j]  # Momentum to velocity
-        self.grid_v[i, j][1] -= dt * 50  # gravity
+        self.grid_v[i, j] = (
+            1 / self.grid_m[i, j]) * self.grid_v[i, j]  # Momentum to velocity
+        self.grid_v[i, j][1] -= self.dt * 50  # gravity
         if i < 3 and self.grid_v[i, j][0] < 0:
           self.grid_v[i, j][0] = 0  # Boundary conditions
-        if i > n_grid - 3 and self.grid_v[i, j][0] > 0:
+        if i > self.res[0] - 3 and self.grid_v[i, j][0] > 0:
           self.grid_v[i, j][0] = 0
         if j < 3 and self.grid_v[i, j][1] < 0:
           self.grid_v[i, j][1] = 0
-        if j > n_grid - 3 and self.grid_v[i, j][1] > 0:
+        if j > self.res[1] - 3 and self.grid_v[i, j][1] > 0:
           self.grid_v[i, j][1] = 0
-    for p in range(n_particles):  # grid to particle (G2P)
-      base = (self.x[p] * inv_dx - 0.5).cast(int)
-      fx = self.x[p] * inv_dx - base.cast(float)
+
+    for p in range(self.n_particles):  # grid to particle (G2P)
+      base = (self.x[p] * self.inv_dx - 0.5).cast(int)
+      fx = self.x[p] * self.inv_dx - base.cast(float)
       w = [
           0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0),
           0.5 * ti.sqr(fx - 0.5)
@@ -109,13 +121,13 @@ class MPMSolver:
         g_v = self.grid_v[base + ti.Vector([i, j])]
         weight = w[i][0] * w[j][1]
         new_v += weight * g_v
-        new_C += 4 * inv_dx * weight * ti.outer_product(g_v, dpos)
+        new_C += 4 * self.inv_dx * weight * ti.outer_product(g_v, dpos)
       self.v[p], self.C[p] = new_v, new_C
-      self.x[p] += dt * self.v[p]  # advection
+      self.x[p] += self.dt * self.v[p]  # advection
 
   def init(self):
-    group_size = n_particles // 3
-    for i in range(n_particles):
+    group_size = self.n_particles // 3
+    for i in range(self.n_particles):
       self.x[i] = [
           random.random() * 0.2 + 0.3 + 0.10 * (i // group_size),
           random.random() * 0.2 + 0.05 + 0.32 * (i // group_size)
@@ -127,11 +139,13 @@ class MPMSolver:
 
 
 gui = ti.GUI("Taichi MLS-MPM-99", res=512, background_color=0x112F41)
-mpm = MPMSolver()
+mpm = MPMSolver(res=(128, 128))
 mpm.init()
+
 for frame in range(20000):
-  for s in range(int(2e-3 // dt)):
+  for s in range(int(2e-3 // mpm.dt)):
     mpm.substep()
   colors = np.array([0x068587, 0xED553B, 0xEEEEF0], dtype=np.uint32)
-  gui.circles(mpm.x.to_numpy(), radius=1.5, color=colors[mpm.material.to_numpy()])
+  gui.circles(
+      mpm.x.to_numpy(), radius=1.5, color=colors[mpm.material.to_numpy()])
   gui.show()  # Change to gui.show(f'{frame:06d}.png') to write images to disk
