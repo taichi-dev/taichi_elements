a3f3b781042686ba809afc116411a33df700dcb1
PavelBlend <stalkermodkytia@yandex.ru>
2020-11-30 21:20:40 +0300

    particles i/o use numpy

--- blender/particles_io.py
+++ blender/particles_io.py
@@ -1,10 +1,15 @@
-import struct
+import struct, os, time
+
+import numpy
+
+import bpy
 
 
 # particles format version 0
 PARS_FMT_VER_0 = 0
+PARS_FMT_VER_1 = 1
 # particles format support versions
-PARS_FMT_SUPP = (PARS_FMT_VER_0, )
+PARS_FMT_SUPP = (PARS_FMT_VER_0, PARS_FMT_VER_1)
 
 # particle attributes
 
@@ -17,11 +22,27 @@ COL = 2
 # material id
 MAT = 3
 
+# numpy attributes type
+attr_types = {
+    POS: numpy.float32,
+    VEL: numpy.float32,
+    COL: numpy.int32,
+    MAT: numpy.int32
+}
+
+# attributes names
+attr_names = {
+    POS: 'pos',
+    VEL: 'vel',
+    COL: 'col',
+    MAT: 'mat'
+}
+
 
 def write_pars_v0(par_data):
     data = bytearray()
     # particles format version
-    data.extend(struct.pack('I', 0))
+    data.extend(struct.pack('I', PARS_FMT_VER_0))
     # particles count
     pars_cnt = len(par_data[POS])
     data.extend(struct.pack('I', pars_cnt))
@@ -33,6 +54,29 @@ def write_pars_v0(par_data):
         data.extend(struct.pack('3f', *par_data[VEL][par_i]))
         data.extend(struct.pack('I', par_data[COL][par_i]))
         data.extend(struct.pack('I', par_data[MAT][par_i]))
+
+    return data
+
+
+def write_pars_v1(par_data, fpath, fname):
+    data = bytearray()
+    # particles format version
+    data.extend(struct.pack('I', PARS_FMT_VER_1))
+    # particles count
+    pars_cnt = par_data[POS].shape[0]
+    data.extend(struct.pack('I', pars_cnt))
+    print('Particles count:', pars_cnt)
+
+    for attr_id in range(4):
+       fname_str = '{}_{}.bin'.format(fname, attr_names[attr_id])
+       fname_byte = bytes(fname_str, 'utf-8')
+       length = len(fname_byte)
+       data.extend(struct.pack('I', length))
+       data.extend(struct.pack('{}s'.format(length), fname_byte))
+
+    for attr_id in range(4):
+        attr_array = par_data[attr_id]
+        attr_array.tofile('{}_{}.bin'.format(fpath, attr_names[attr_id]))
 
     return data
 
@@ -74,8 +118,30 @@ def read_pars_v0(data, caches, offs, folder):
     caches[folder] = {POS: pos, VEL: vel, COL: col, MAT: mat}
 
 
+def read_pars_v1(data, caches, offs, folder):
+    # particles count
+    count = struct.unpack('I', data[offs : offs + 4])[0]
+    offs += 4
+    caches[folder] = {}
+
+    for attr_id in range(4):
+        file_name_len = struct.unpack('I', data[offs : offs + 4])[0]
+        offs += 4
+
+        file_name_bytes = struct.unpack('{}s'.format(file_name_len), data[offs : offs + file_name_len])[0]
+        file_name = str(file_name_bytes, 'utf-8')
+        offs += file_name_len
+
+        file_path = bpy.path.abspath(os.path.join(folder, file_name))
+        caches[folder][attr_id] = numpy.fromfile(file_path, dtype=attr_types[attr_id])
+
+
 # read particles
-def read_pars(data, caches, folder):
+def read_pars(file_path, caches, folder, attr_name):
+    start_time = time.time()
+
+    with open(file_path, 'rb') as file:
+        data = file.read()
     # read offset in file
     offs = 0
     # particles format version
@@ -88,3 +154,9 @@ def read_pars(data, caches, folder):
 
     if ver == PARS_FMT_VER_0:
         read_pars_v0(data, caches, offs, folder)
+    elif ver == PARS_FMT_VER_1:
+        read_pars_v1(data, caches, offs, folder)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print('read particles {}: {:.4f} seconds'.format(attr_name.lower(), total_time))
