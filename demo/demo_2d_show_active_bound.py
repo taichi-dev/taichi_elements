ab5fc40b5e655ab7ae6b8a0b43b7ec428367abad
jack12xl <jack12xieling@163.com>
2021-08-17 10:41:08 +0800

    update active bound

--- demo/demo_2d_show_active_bound.py
+++ demo/demo_2d_show_active_bound.py
@@ -6,6 +6,7 @@ import math
 from engine.mpm_solver import MPMSolver
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--out-dir', type=str, help='Output folder')
@@ -13,56 +14,38 @@ def parse_args():
     print(args)
     return args
 
+
 args = parse_args()
 
 write_to_disk = args.out_dir is not None
 if write_to_disk:
     os.mkdir(f'{args.out_dir}')
 
-ti.init(arch=ti.cuda, debug=True)  # Try to run on GPU
+ti.init(arch=ti.cuda, debug=False)  # Try to run on GPU
 
 n = 256
 gui = ti.GUI("Taichi Elements", res=n, background_color=0x112F41)
 
-
 activate_vis = ti.field(dtype=ti.f32, shape=[n, n])
 mpm = MPMSolver(res=(n, n))
-
 
 for i in range(3):
     mpm.add_cube(lower_corner=[0.2 + i * 0.1, 0.3 + i * 0.1],
                  cube_size=[0.1, 0.1],
                  material=MPMSolver.material_elastic)
 
-kernel_size = n // n
-
 @ti.kernel
-def test_active(vs_field:ti.template(), solver:ti.template()):
+def block_active(vs_field: ti.template(), solver: ti.template()):
     for I in ti.grouped(vs_field):
+        blk_I = I // solver.leaf_block_size - solver.block_offset
+        # print(blk_I, I)
+        if ti.is_active(solver.block, blk_I):
 
-        m_sum = 0.0
-        for i in ti.static(range(kernel_size)):
-            for j in ti.static(range(kernel_size)):
-                idx = kernel_size * I + ti.Vector([i, j])
-                m_sum += solver.grid_m[idx]
-
-        if m_sum > 1e-6:
-            vs_field[I] = 1.
-            # print("act ", I)
+            vs_field[I] = 1.0
         else:
-            vs_field[I] = 0.0
-            # print("no act ", I)
+            vs_field[I] = 0.8
 
-@ti.kernel
-def test_block_active(vs_field:ti.template(), solver:ti.template()):
-    for I in ti.grouped(vs_field):
-        tmp = ti.is_active(solver.block, I)
-        # if (tmp > 0):
-        #     print(I, tmp)
-        vs_field[I] = tmp
 
-print(mpm.test_grid.shape)
-print(mpm.block.shape)
 for frame in range(500):
     mpm.step(8e-3)
     if frame < 500:
@@ -84,13 +67,13 @@ for frame in range(500):
     colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
                       dtype=np.uint32)
     particles = mpm.particle_info()
-    # gui.circles(particles['position'],
-    #             radius=1.5,
-    #             color=colors[particles['material']])
-    # gui.show(f'{args.out_dir}/{frame:06d}.png' if write_to_disk else None)
-    # test_active(activate_vis, mpm)
-    # gui.set_image(activate_vis)
-    # gui.show(f'{args.out_dir}/{frame:06d}.png' if write_to_disk else None)
-    test_block_active(activate_vis, mpm)
+    gui.circles(particles['position'],
+                radius=1.5,
+                color=colors[particles['material']])
+
+    block_active(activate_vis, mpm)
     gui.set_image(activate_vis)
+    gui.circles(particles['position'],
+                radius=1.5,
+                color=colors[particles['material']])
     gui.show(f'{args.out_dir}/{frame:06d}.png' if write_to_disk else None)
